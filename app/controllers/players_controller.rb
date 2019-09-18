6e977a28ab4df789ebdc0b5d4c197ce889276a31
Chris Cardle <cdcardle@gmail.com>
2019-09-18 11:12:12 -0400

    added edit form and routes for editing players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -7,4 +7,19 @@ class PlayersController < ApplicationController
       redirect '/'
     end
   end
+
+  get "/:slug/teams/:team_id/players/:player_id/edit" do
+    @player = Player.find(params[:player_id])
+    @user = @player.user
+    @team = @player.team
+    erb :'players/edit'
+  end
+  
+  patch "/:slug/teams/:team_id/players/:player_id" do
+    @player = Player.find(params[:player_id])
+    @player.first_name = params[:first_name]
+    @player.last_name = params[:last_name]
+    @player.save
+    redirect to "/#{current_user.slug}/teams/#{params[:team_id]}"
+  end
 end
