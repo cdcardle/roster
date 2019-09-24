151edf5eff1baf66150f2666d1005fa957a7d117
Chris Cardle <cdcardle@gmail.com>
2019-09-24 15:20:34 -0400

    finished tests for application controller and added some for user

--- spec/application_controller_spec.rb
+++ spec/application_controller_spec.rb
@@ -24,3 +24,55 @@ describe '/' do
     expect(last_response.header['Location']).to eq('http://example.org/nightmoves')
   end
 end
+
+describe '/signup' do
+  it "get should display signup page" do
+    get '/signup'
+
+    expect(last_response).to be_ok
+    expect(last_response.body).to include("Create an account to get started!")
+  end
+
+  it "post should create user after form submit and redirect to /:slug" do
+    post '/signup', {
+      first_name: "Bob", last_name: "Seger", email: "silverbullet1945@hotmail.com", username: "nightmoves", password: "mainstreet"
+    }
+
+    expect(User.find_by(username: "nightmoves")).to be_truthy
+    expect(last_response.status).to eq(302)
+    expect(last_response.header['Location']).to eq("http://example.org/nightmoves")
+  end
+end
+
+describe '/login' do
+  it "get should display login page" do
+    get '/login'
+
+    expect(last_response).to be_ok
+    expect(last_response.body).to include("Log in to view your teams!")
+  end
+
+  it "post should set session[:user_id] and redirect to /:slug" do
+    @bob = User.create(first_name: "Bob", last_name: "Seger", email: "silverbullet1945@hotmail.com", username: "nightmoves", password: "mainstreet")
+    post '/login', {
+      username: "nightmoves", password: "mainstreet"
+    }
+
+    expect(session[:user_id]).to eq(@bob.id)
+    expect(last_response.status).to eq(302)
+    expect(last_response.header['Location']).to eq("http://example.org/nightmoves")
+  end
+end
+
+describe '/logout' do
+  it "clears the session and redirects to /" do
+    @bob = User.create(first_name: "Bob", last_name: "Seger", email: "silverbullet1945@hotmail.com", username: "nightmoves", password: "mainstreet")
+    post '/login', {
+      username: "nightmoves", password: "mainstreet"
+    }
+
+    get '/logout'
+    
+    expect(session[:user_id]).to eq(nil)
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,38 @@ require 'spec_helper'
 
 describe "User" do
   before do
-    @user = User.create(first_name: "Bob", last_name: "Seger", email: "silverbullet1945@hotmail.com", username: "nightmoves", password: "mainstreet")
+    @bob = User.create(first_name: "Bob", last_name: "Seger", email: "silverbullet1945@hotmail.com", username: "nightmoves", password: "mainstreet")
+    @joe = User.new(email: "joesatriani@gmail.com", first_name: "Joe", last_name: "Satriani", username: "joesatriani", password: "joespassword")
   end
 
-  it 'has a secure password' do
-    expect(@user.authenticate("agaisntthewind")).to eq(false)
-    expect(@user.authenticate("mainstreet")).to eq(@user)
+  describe "password" do
+    it 'has a secure password' do
+      expect(@bob.authenticate("agaisntthewind")).to eq(false)
+      expect(@bob.authenticate("mainstreet")).to eq(@bob)
+    end
+
+    it "is at least 8 characters long" do
+      @joe.password = "joe"
+      @bob.password = "bobspassword"
+
+      expect(@joe.save).to eq(false)
+      expect(@bob.save).to eq(true)
+    end
+  end
+
+  describe "email" do
+    it 'has an email' do
+      @joe.email = ''
+      @bob.email = 'againstthewind@yahoo.com'
+
+      expect(@joe.save).to eq(false)
+      expect(@bob.save).to eq(true)
+    end
+
+    it 'has a unique email' do
+      @joe.email = "silverbullet1945@hotmail.com"
+
+      expect(@joe.save).to eq(false)
+    end
   end
 end
