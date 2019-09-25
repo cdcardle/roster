e60a0670ec4fdbd26998cd7049e9029057a64411
Chris Cardle <cdcardle@gmail.com>
2019-09-25 11:55:31 -0400

    added team_spec file and updated specmd

---
+++ spec/models/team_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe "Team" do
+  before do
+    @bob = User.create(first_name: "Bob", last_name: "Seger", email: "silverbullet1945@hotmail.com", username: "night moves", password: "mainstreet")
+  end
+end
