4d786d5325951bd3ef736d1420dec7d8a6b79a39
Chris Cardle <cdcardle@gmail.com>
2019-09-27 12:13:52 -0400

    added route tests for /:slug for users_controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,10 +1,35 @@
 require "spec_helper"
 
-@bob = User.create(first_name: "Bob", last_name: "Seger", email: "silverbullet1945@hotmail.com", username: "nightmoves", password: "mainstreet")
-
 describe "/:slug" do
-  it "redirects to '/' if not logged in" do
-    get '/nightmoves'
-    expect(last_response.header['Location']).to eq("http://example.org/")
+  describe "get" do
+    it "redirects to '/' if not logged in" do
+      get '/nightmoves'
+      expect(last_response.header['Location']).to eq("http://example.org/")
+    end
+
+    it "displays a list of teams if logged in" do
+      @bob = User.create(first_name: "Bob", last_name: "Seger", email: "silverbullet1945@hotmail.com", username: "nightmoves", password: "mainstreet")
+      @tigers = Team.create(name: "Tigers")
+      @bob.teams << @tigers
+
+      post '/login', {username: "nightmoves", password: "mainstreet"}
+
+      get '/nightmoves'
+
+      expect(last_response.body).to include("Tigers")
+    end
+  end
+
+  describe 'post' do
+    it "creates a new team and redirects to /:slug" do
+      @bob = User.create(first_name: "Bob", last_name: "Seger", email: "silverbullet1945@hotmail.com", username: "nightmoves", password: "mainstreet")
+
+      post '/login', {username: "nightmoves", password: "mainstreet"}
+      post '/nightmoves', {name: "Badgers"}
+      follow_redirect!
+
+      expect(Team.find_by(name: "Badgers").valid?).to be(true)
+      expect(last_response.body).to include("Badgers")
+    end
   end
 end
